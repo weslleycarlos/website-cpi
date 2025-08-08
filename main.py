eaa879a8debe5428c81d4b2bde0ce48ab527d904
Weslley Carlos <weslley.unemat@gmail.com>
2025-08-08 14:39:55 -0300

    refactor: Substitui formulário de contato por seção de ícones sociais

--- main.py
+++ main.py
@@ -7,25 +7,6 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-# Nova rota para lidar com o formulário
-@app.route('/enviar_contato', methods=['POST'])
-def enviar_contato():
-    # A mágica acontece aqui!
-    if request.method == 'POST':
-        nome = request.form['nome']
-        email = request.form['email']
-        mensagem = request.form['mensagem']
-        
-        # POR ENQUANTO: Vamos apenas imprimir no terminal para ver se funcionou
-        print(f"Nova mensagem de contato:")
-        print(f"Nome: {nome}")
-        print(f"Email: {email}")
-        print(f"Mensagem: {mensagem}")
-        
-        # Futuramente, aqui você pode adicionar o código para enviar um e-mail.
-        
-        # Redireciona o usuário de volta para a página inicial
-        return redirect('/#contato')
 
 if __name__ == '__main__':
     app.run(debug=True)
